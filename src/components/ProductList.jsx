66b3f9364888b0d0204ff81354014b1dfd4c2f77
matthias.sforza <matthias.sforza@gmail.com>
2024-06-14 13:39:09 +0200

    ProductList commit

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,13 @@
+import Product from './Product.jsx';
+
+//envoi de la prop products histoire de pouvoir render la liste
+const ProductList = ({ products }) => (
+  <div>
+    {products.map((product, index) => (
+      <Product key={index} {...product} /> 
+    ))}
+    {/*spread pour envoyer toutes les données sans code à rallonge*/}
+  </div>
+);
+
+export default ProductList;
