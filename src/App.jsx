4194137a09587848db57267d9fd2b2b7ddfbc418
matthias.sforza <matthias.sforza@gmail.com>
2024-06-14 15:26:26 +0200

    App finished & CSS product modif

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import React from "react"
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState, useEffect } from 'react';
+import ProductList from './components/ProductList.jsx';
+import ProductForm from './components/ProductForm.jsx';
+import './App.css';
 
 
-function App() {
- 
+const App = () => {
+  // chargement de la liste de produit et sauvegarde
+  const [products, setProducts] = useState(() => {
+    const savedProducts = localStorage.getItem('products');
+    return savedProducts ? JSON.parse(savedProducts) : [];
+  });
+
+  //localstorage pour les produits
+  useEffect(() => {
+    localStorage.setItem('products', JSON.stringify(products));
+  }, [products]);
+
+
+  //ajout d'un produit
+  const addProduct = (newProduct) => {
+    setProducts([...products, newProduct]);
+  };
 
   return (
-    <>
-    </>
-  )
-}
+    <div className="app">
+      <h1 className="titre">Liste de produits du marché</h1>
+      <ProductForm addProduct={addProduct} />
+      <ProductList products={products} />
+    </div>
+  );
+};
 
-export default App
+export default App;
