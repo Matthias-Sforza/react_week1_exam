aac9d9507014a5bac2a2c4c27f32fcad63aef4f6
matthias.sforza <matthias.sforza@gmail.com>
2024-06-14 13:00:47 +0200

    ajout CSS et commentaires pour clarté

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
 import './Product.css';
 
+
+//Déclaration du component Product avec destructuring comme dans les exos précédents
 const Product = ({ name, description, price }) => (
   <div className="produit">
     <h2>{name}</h2>
     <p>{description}</p>
-    <p className="thunes">${price.toFixed(2)}</p>
+    <p className="thunes">${price.toFixed(2)}</p> {/*toFixed avec 2 ici pour les centimes/cents*/}
+    
   </div>
 );
 
